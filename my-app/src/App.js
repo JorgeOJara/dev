df3d9ae5823b704a16f96ab33655f9cc0346dbf1
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-01 18:16:09 -0400

    'Something'

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,67 +7,61 @@ import './form.css';
 
 
 function App() {
-
+  // hook for singup/login
 const [Changes,setChanges] =  useState("container")
 
-function singUpButton(){
-     setChanges("container right-panel-active")
-  }
-
-  function singINButton(){
-      setChanges("container")
-  }
+function singUpButton(){setChanges("container right-panel-active")}
+function singINButton(){setChanges("container")}
 
 
 return (
     <div className="App">
-     <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
-<div className={Changes} id="container">
-  <div class="form-container sign-up-container">
-    <form action="#">
-      <h1>Create Account</h1>
-      <div class="social-container">
-        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-      </div>
-      <span>or use your email for registration</span>
-      <input type="text" placeholder="Name" />
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button>Sign Up</button>
-    </form>
+      <form class="form">
+  <div class="control">
+    <h1>
+      Sign In
+    </h1>
   </div>
-  <div class="form-container sign-in-container">
-    <form action="#">
-      <h1>Sign in</h1>
-      <div class="social-container">
-        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-      </div>
-      <span>or use your account</span>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <a href="#">Forgot your password?</a>
-      <button>Sign In</button>
-    </form>
-  </div>
-  <div class="overlay-container">
-    <div class="overlay">
-      <div class="overlay-panel overlay-left">
-        <h1>Welcome Back!</h1>
-        <p>To keep connected with us please login with your personal info</p>
-        <button onClick={ singINButton } class="ghost" id="signIn">Sign In</button>
-      </div>
-      <div class="overlay-panel overlay-right">
-        <h1>Hello, Friend!</h1>
-        <p>Enter your personal details and start journey with us</p>
-        <button onClick={singUpButton} class="ghost" id="signUp">Sign Up</button>
-      </div>
+  <div class="control block-cube block-input">
+    <input></input><div class="bg-top">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="bg-right">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="bg">
+      <div class="bg-inner"></div>
     </div>
   </div>
-</div>
+  <div class="control block-cube block-input">
+    <input ></input>
+      <div class="bg-top">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="bg-right">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="bg">
+      <div class="bg-inner"></div>
+    </div>
+  </div>
+  <button class="btn block-cube block-cube-hover">
+    <div class="bg-top">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="bg-right">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="bg">
+      <div class="bg-inner"></div>
+    </div>
+    <div class="text">
+      Log In
+    </div></button>
+  <div class="credits">
+    <a>
+      My other codepens</a>
+  </div></form>
    </div>
   );
 }
