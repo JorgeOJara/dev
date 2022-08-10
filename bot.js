c5581ee23ad89c2d38511945241cc94be62ac9ca
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-09 21:09:49 -0400

    'Something'

---
+++ bot.js
@@ -0,0 +1,27 @@
+// Initialize dotenv
+require('dotenv').config();
+
+// Discord.js versions ^13.0 require us to explicitly define client intents
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: '' });
+
+    
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+// client.on("message", msg => {
+//   if (msg.content === "ping") {
+//     msg.reply("pong");
+//   }
+// })
+client.on('messageCreate', msg => {
+// You can view the msg object here with console.log(msg)
+ if (msg.content === 'hello') {
+   msg.reply(`Hello ${msg.author.username}`);
+ }
+});
+
+// Log In our bot
+client.login('MTAwNjY4MDY1ODE5NTc5NjAxOA.G0wsKT.85KodqcH5dncVVJZWZjwtcJuktk7hpYJ8GzPLM');
