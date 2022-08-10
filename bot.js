0bbf80dfac82846d68e57923e4be64f6ccceec0d
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-10 15:44:18 -0400

    'Something'

--- bot.js
+++ bot.js
@@ -2,18 +2,13 @@ require('dotenv').config();
 // const cleverbot require("cleverbot-free");
 const { Client, Intents, GatewayIntentBits} = require('discord.js');
 
-// testing
-const smartestchatbot = require('smartestchatbot');
-const chater = new smartestchatbot.Client();
-
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildPresences,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessageReactions
-    ],
-    partials: ['CHANNEL', 'MESSAGE', 'REACTION']
+    ],partials: ['CHANNEL', 'MESSAGE', 'REACTION']
    });
 
 client.on('ready', () => {
@@ -26,16 +21,12 @@ client.on('messageCreate', (message) => {
   if (message.mentions.has(client.user.id)) {
         console.log(message.content);
         message.reply("stop texting, nobody cares..");
-    chater.chat({message: message, name:"SmartestChatbot", owner:message.author.username, user: message.author.id}, "en").then(reply => {
-    message.reply(reply)
-      });
     }
     
     if(message.author != message.author.bot){
         console.log(message)
          message.reply('no one cares..'+ message.author.username)
     }
-
 });
 
 // Log In our bot
