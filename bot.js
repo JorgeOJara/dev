41be1941b3ec3b67de9be487fd31e87ec60a1ba6
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-09 23:03:49 -0400

    'Something'

--- bot.js
+++ bot.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 // const cleverbot require("cleverbot-free");
 const { Client, Intents, GatewayIntentBits} = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS , Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [
+    Intents.FLAGS.GUILDS ,
+    Intents.FLAGS.GUILD_MESSAGES,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions]
+   });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
