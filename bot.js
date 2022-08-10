4f41b088c7dec820beb5a76379713db2ff58370d
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-10 18:12:28 -0400

    'Something'

--- bot.js
+++ bot.js
@@ -1,35 +1,52 @@
 require('dotenv').config();
 // const cleverbot require("cleverbot-free");
-const { Client, Intents, GatewayIntentBits} = require('discord.js');
+// const { Client, Intents, GatewayIntentBits} = require('discord.js');
 
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.GuildBans,
-    ],partials: ['CHANNEL', 'MESSAGE', 'REACTION']
-   });
+// const client = new Client({ intents: [
+//     GatewayIntentBits.Guilds,
+//     GatewayIntentBits.GuildMessages,
+//     GatewayIntentBits.GuildPresences,
+//     GatewayIntentBits.GuildMembers,
+//     GatewayIntentBits.GuildMessageReactions,
+//     GatewayIntentBits.DirectMessages,
+//     GatewayIntentBits.GuildBans,
+//     ],partials: ['CHANNEL', 'MESSAGE', 'REACTION']
+//    });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
 
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return;
+// client.on('messageCreate', (message) => {
+//   if (message.author.bot) return;
 
-  if (message.mentions.has(client.user.id)) {
-        console.log(message.content);
-        message.reply("stop texting, nobody cares..");
-    }
+//   if (message.mentions.has(client.user.id)) {
+//         console.log(message.content);
+//         message.reply("stop texting, nobody cares..");
+//     }
     
-    if(message.author != message.author.bot){
-        console.log(message)
-         message.reply('no one cares..'+ message.author.username)
-    }
+//     if(message.author != message.author.bot){
+//         console.log(message)
+//          message.reply('no one cares..'+ message.author.username)
+//     }
+// });
+
+
+
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+
+const bot = new Client({
+  'intents': [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages
+  ],
+  'partials': [Partials.Channel]
 });
 
+bot.on('messageCreate', async (message) => {
+  console.log(message);
+});
 // Log In our bot
-client.login(process.env.CLIENT_TOKEN);
+bot.login(process.env.CLIENT_TOKEN);
