4a58859a065bf153246dad244778bc1de579643b
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-10 15:05:31 -0400

    'Something'

--- bot.js
+++ bot.js
@@ -15,33 +15,20 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// client.on('messageCreate', (message) => {
-//   if (message.author.bot) return;
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
 
-//   if (message.mentions.has(client.user.id)) {
-//         console.log(message.content);
-//         message.reply("dont care..");
-//     }
+  if (message.mentions.has(client.user.id)) {
+        console.log(message.content);
+        message.reply("dont care..");
+    }
     
 
-//     if(message.author != message.author.bot){
-//         console.log(message)
+    if(message.author != message.author.bot){
+        console.log(message)
 
-//          message.reply('no one cares..'+ message.author.username)
-//     }
-// });
-
-//Listen to new messages on the server
-client.on("messageCreate", async (message) => {
-  if (message.content === "ping" || message.author != message.author.bot) {
-    message.reply("pong");
-  }
-
-   // if(message.author != message.author.bot){
-   //      console.log(message)
-
-   //       message.reply('no one cares..'+ message.author.username)
-   //  }
+         message.reply('no one cares..'+ message.author.username)
+    }
 });
 
 // Log In our bot
