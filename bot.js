aed026909d1aeb75ddb01944e4801b2d6a6bad2b
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-11 15:47:24 -0400

    'Something'

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('messageCreate', (message) => {
     //  reply if you say something
 
     if(message.author != message.author.bot){
-        console.log(message.attachments);
+        console.log(message.attachments.Collection.url);
         message.reply("Stop talking...");
     }
 });
