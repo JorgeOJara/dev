706fffeeabc783088989a026132a2d801ac3047d
hopeonsomethings <hopeonsomethings@gmail.com>
2022-08-14 00:31:04 -0400

    'Something'

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ if(message.content.startsWith("?roll")){
                     completed.push(num);
                     total += num;
                }
-      if(final.length > 2 )
+      if(final.length  == 2 )
       {
          if(final[2].startsWith("+")){
                let taketheOperationOut = final[2].replace("+");
@@ -67,9 +67,7 @@ if(message.content.startsWith("?roll")){
                let add  =  total + mkNumber;
                    message.reply(completed.toString() + " Total : " + add );
                    console.log("add")
-            }
-            else
-            {
+            }else{
                let taketheOperationOut = final[2].replace("-");
                let mkNumber = Number(taketheOperationOut);
                let resValue  =  total - mkNumber;
